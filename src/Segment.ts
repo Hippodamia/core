96ad054720cda53cfbdded0a71d5198f01e9c394
“heerheer” <1767407822@qq.com>
2024-04-05 15:45:34 +0800

    changed

--- src/Segment.ts
+++
@@ -1,9 +0,0 @@
-import { BuffContainer } from "./BuffContainer";
-import { Track } from "./Track";
-
-export class Segment {
-  constructor() {
-    this.buffs = new BuffContainer<any>();
-  }
-  buffs: BuffContainer<any>;
-}
